65ea606601db8457f60ad5d30f61417ced72c9ec
Roman Gruba <roman.gruba@gmail.com>
2019-08-01 21:24:24 +0300

    for review

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './styles.css';
+
+const body = document.querySelector('body');
+
+const buttonStart = document.createElement('button');
+buttonStart.type = 'button';
+buttonStart.dataset.action = 'start';
+buttonStart.textContent = 'Start';
+
+const buttonStop = document.createElement('button');
+buttonStop.type = 'button';
+buttonStop.dataset.action = 'stop';
+buttonStop.textContent = 'Stop';
+
+body.prepend(buttonStart, buttonStop);
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changeColor() {
+  body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+}
+
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(changeColor, 1000);
+  buttonStart.disabled = true;
+  buttonStart.style.color = '#FF0000';
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStart.disabled = false;
+  buttonStart.style.color = '#FFFFFF';
+});
